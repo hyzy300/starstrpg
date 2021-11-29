7a073d39a63f312f7fdefba3cd0ade40d4fb6e02
harris <1159430071@qq.com>
2021-11-29 15:46:34 +0800

    11.29.15:30

---
+++ src/components/homepage.vue
@@ -0,0 +1,32 @@
+<template>
+  <body>
+    <span></span>
+    <div id="app">
+      <div class="home">
+        <div class="header">
+          <div class="first"><slot class="first"></slot></div>
+          <div class="second"><slot class="second"></slot></div>
+          <div class="third"><slot class="third"></slot></div>
+        </div>
+      </div>
+    </div>
+  </body>
+</template>
+
+<script>
+export default {
+  name
+};
+</script>
+
+<style>
+  .header {
+    display: flex;
+    line-height: 64px;
+  }
+  .first,.second{
+    width: 60px;
+    background-color: darkblue;
+  }
+
+</style>
